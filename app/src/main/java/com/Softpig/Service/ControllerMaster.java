9fc543c93359db1018164b890e47784dd72acd45
fernandoromero <fernandoromero@wposs.com>
2019-09-06 08:04:03 -0500

    Subida de ControllerMaster

---
+++ app/src/main/java/com/Softpig/Service/ControllerMaster.java
@@ -0,0 +1,11 @@
+package com.Softpig.Service;
+
+import android.view.View;
+
+public class ControllerMaster {
+
+
+
+    public static void login(View.OnClickListener onClickListener, String codeUser, String password) {
+    }
+}
