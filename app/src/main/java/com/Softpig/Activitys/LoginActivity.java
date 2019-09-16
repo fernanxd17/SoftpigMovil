7884b547677a9a9f4415b03c5013b59ba56b2e4d
Fernando Romero <juanfernandoro@ufps.edu.co>
2019-09-15 21:35:13 -0500

    layouts: about, login, main, y se da inicio a algunos metodos

--- app/src/main/java/com/Softpig/Activitys/LoginActivity.java
+++ app/src/main/java/com/Softpig/Activitys/LoginActivity.java
@@ -2,8 +2,11 @@ package com.Softpig.Activitys;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -21,29 +24,19 @@ public class LoginActivity extends AppCompatActivity {
     private static EditText etPassword;
     private static Button btLogin;
     private static TextView etForgetPassword;
+    private static TextView etDictionary;
+    private static TextView etAbout;
     private ControllerMaster controllerMaster;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
-
-        this.etCodeUser = (EditText)findViewById(R.id.et_codeuser);
-        this.etPassword = (EditText)findViewById(R.id.et_password);
-        this.btLogin = (Button)findViewById(R.id.bt_loginup);
-        this.etForgetPassword = (TextView)findViewById(R.id.et_forgetpassword);
-
         this.controllerMaster = new ControllerMaster();
+        iniciarCampos();
+
 
         this.btLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-            }
-        });
-
-
-        this.etForgetPassword.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String codeUser = LoginActivity.this.etCodeUser.getText().toString();
@@ -58,12 +51,60 @@ public class LoginActivity extends AppCompatActivity {
                 }
             }
         });
+
+        this.etDictionary.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent dictionary = new Intent();
+                dictionary.setClass(LoginActivity.this, DictionaryActivity.class);
+                startActivity(dictionary);
+            }
+        });
+
+        this.etAbout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent about = new Intent();
+                about.setClass(LoginActivity.this, AboutActivity.class);
+                startActivity(about);
+            }
+        });
+
+
+
     }
 
+    private void iniciarCampos() {
+        this.etCodeUser = (EditText)findViewById(R.id.et_code_user);
+        this.etPassword = (EditText)findViewById(R.id.et_password);
+        this.btLogin = (Button)findViewById(R.id.bt_loginup);
+        this.etForgetPassword = (TextView)findViewById(R.id.et_forget_password);
+        this.etDictionary = (TextView)findViewById(R.id.tv_dictionary);
+        this.etAbout = (TextView)findViewById(R.id.tv_about);
+    }
 
 
     private void login(String codeUser, String password){
         JSONObject datos = LoginActivity.this.controllerMaster.login(codeUser, password);
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            hideSystemUI();
+        }
+    }
+
+    private void hideSystemUI() {
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN);
     }
 
 }
