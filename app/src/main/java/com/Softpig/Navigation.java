c791348a8629abbe5bfb2ddab4076047bb285bc0
Paola Pajaro <yindypaolapu@ufps.edu.co>
2019-10-09 14:09:45 -0500

    Trabajando en la visualización del bottombar

--- app/src/main/java/com/Softpig/Navigation.java
+++ app/src/main/java/com/Softpig/Navigation.java
@@ -7,10 +7,10 @@ import com.google.android.material.snackbar.Snackbar;
 
 import android.view.View;
 
-import androidx.navigation.NavController;
+/**import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
+import androidx.navigation.ui.NavigationUI;**/
 
 import com.google.android.material.navigation.NavigationView;
 
@@ -23,7 +23,7 @@ import android.view.Menu;
 
 public class Navigation extends AppCompatActivity {
 
-    private AppBarConfiguration mAppBarConfiguration;
+    //private AppBarConfiguration mAppBarConfiguration;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,14 +43,14 @@ public class Navigation extends AppCompatActivity {
         NavigationView navigationView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        mAppBarConfiguration = new AppBarConfiguration.Builder(
+       /** mAppBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow,
                 R.id.nav_tools, R.id.nav_share, R.id.nav_send)
                 .setDrawerLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
-        NavigationUI.setupWithNavController(navigationView, navController);
+        NavigationUI.setupWithNavController(navigationView, navController);**/
     }
 
     @Override
@@ -62,8 +62,9 @@ public class Navigation extends AppCompatActivity {
 
     @Override
     public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        /**NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
-                || super.onSupportNavigateUp();
+                || super.onSupportNavigateUp();**/
+        return false;
     }
 }
