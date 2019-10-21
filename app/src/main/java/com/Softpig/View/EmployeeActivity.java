216e99ed88460e223c9db81f2405ec665021ef82
Fernando Romero <juanfernandoro@ufps.edu.co>
2019-10-21 10:58:05 -0500

    arreglando manifest

--- app/src/main/java/com/Softpig/View/EmployeeActivity.java
+++ app/src/main/java/com/Softpig/View/EmployeeActivity.java
@@ -17,33 +17,28 @@ import java.util.Date;
 
 public class EmployeeActivity extends AppCompatActivity {
 
-    ArrayList<Employee> listEmployee;
-    RecyclerView recyclerEmployee;
-    EmployeePresenter employeePresenter;
+    private ArrayList<Employee> listEmployee;
+    private RecyclerView recyclerEmployee;
+    private EmployeePresenter employeePresenter;
+    private EmployeeAdapter employeeAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.employees);
         employeePresenter = new EmployeePresenter();
-        listEmployee = new ArrayList<Employee>();
+        listEmployee = new ArrayList<>();
         recyclerEmployee = findViewById(R.id.recyclerEmployee);
         recyclerEmployee.setLayoutManager(new LinearLayoutManager(this));
-                showEmployees();
-
+        showEmployees();
+        employeeAdapter = new EmployeeAdapter(listEmployee);
+        recyclerEmployee.setAdapter(employeeAdapter);
     }
 
     private void showEmployees() {
         listEmployee = employeePresenter.getEmployees();
-        EmployeeAdapter employeeAdapter = new EmployeeAdapter(listEmployee);
-        this.recyclerEmployee.setAdapter(employeeAdapter);
+        //employeeAdapter = new EmployeeAdapter(listEmployee);
+        //recyclerEmployee.setAdapter(employeeAdapter);
+   }
 
-
-
-
-        /*Date fecha = new Date();
-        listEmployee.add(new Employee((short) 01, "Administrador", "Activo",fecha , "1090512864", "Masculino", "Eduardo", "Jose", "Pajaro", "Caballero", "eduardojosepc@ufps.edu.co", "55555","3504018064"));
-        listEmployee.add(new Employee((short) 01, "Administrador", "Activo",fecha , "1090512864", "Masculino", "Eduardo", "Jose", "Pajaro", "Caballero", "eduardojosepc@ufps.edu.co", "55555","3504018064"));
-        listEmployee.add(new Employee((short) 01, "Administrador", "Activo",fecha , "1090512864", "Masculino", "Eduardo", "Jose", "Pajaro", "Caballero", "eduardojosepc@ufps.edu.co", "55555","3504018064"));*/
-    }
 }
