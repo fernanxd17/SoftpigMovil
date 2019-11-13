08f657c6a2d6237c43d111f4fe552bd119e28b79
fernandoromero <fernandoromero@wposs.com>
2019-11-13 10:42:22 -0500

    infopigfragment

--- app/src/main/java/com/Softpig/View/fragment/InfoPigFragment.java
+++ app/src/main/java/com/Softpig/View/fragment/InfoPigFragment.java
@@ -29,7 +29,7 @@ public class InfoPigFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         TextView textView = new TextView(getActivity());
-        textView.setText(R.string.hello_blank_fragment);
+        textView.setText("");
         return textView;
     }
 
