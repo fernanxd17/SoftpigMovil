6b88b5e0fac2ef93e7851e93be6a38f65130c136
jerson ortiz <jersonandresoc@ufps.edu.co>
2019-11-13 10:03:08 -0500

    softpig paula

---
+++ app/src/main/java/com/Softpig/Models/PeriodGestation.java
@@ -0,0 +1,34 @@
+package com.Softpig.Models;
+
+import java.util.Date;
+
+public class PeriodGestation {
+
+    private short idPeriodGestation;
+    private short idFemale;
+    private short idMale;
+    private Date dateStart;
+
+    public PeriodGestation(short idPeriodGestation, short idFemale, short idMale, Date dateStart) {
+        this.idPeriodGestation = idPeriodGestation;
+        this.idFemale = idFemale;
+        this.idMale = idMale;
+        this.dateStart = dateStart;
+    }
+
+    public short getIdPeriodGestation() {
+        return idPeriodGestation;
+    }
+
+    public short getIdFemale() {
+        return idFemale;
+    }
+
+    public short getIdMale() {
+        return idMale;
+    }
+
+    public Date getDateStart() {
+        return dateStart;
+    }
+}
