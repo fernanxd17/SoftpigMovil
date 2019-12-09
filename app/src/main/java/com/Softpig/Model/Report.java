caef0e01c0b130eca87e9ba603a2ad95b115f445
Paola Pajaro <yindypaolapu@ufps.edu.co>
2019-12-09 07:57:06 -0500

    Trabajando en generar el PDF

---
+++ app/src/main/java/com/Softpig/Model/Report.java
@@ -0,0 +1,16 @@
+package com.Softpig.Model;
+
+import android.content.Context;
+import android.os.Environment;
+
+public class Report {
+    private Context context;
+
+    public Report(Context context){
+        this.context = context;
+    }
+
+    private void createFile(){
+       // File folder = new File (Environment.getExternalStorageDirectory().toString(), "Reporte_")
+    }
+}
